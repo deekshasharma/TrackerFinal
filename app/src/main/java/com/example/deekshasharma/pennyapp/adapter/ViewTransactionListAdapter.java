88aefb05f909be5069d0f5ab2dd04d2e784607bb
Deeksha <deeksha.sharma25@gmail.com>
2015-03-10 19:24:09 -0700

    ViewTransactions now shows proper date format

--- app/src/main/java/com/example/deekshasharma/pennyapp/adapter/ViewTransactionListAdapter.java
+++ app/src/main/java/com/example/deekshasharma/pennyapp/adapter/ViewTransactionListAdapter.java
@@ -13,14 +13,20 @@ import com.example.deekshasharma.pennyapp.R;
 import com.example.deekshasharma.pennyapp.model.GroupToImage;
 import com.example.deekshasharma.pennyapp.model.TransactionItem;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class ViewTransactionListAdapter extends ArrayAdapter {
 
     private List<TransactionItem> transactionItemList;
+    private Context context;
 
     public ViewTransactionListAdapter(Context context, int resource, List<TransactionItem> transactionItemList) {
         super(context, resource, transactionItemList);
+        this.context = context;
         this.transactionItemList = transactionItemList;
     }
 
@@ -38,8 +44,10 @@ public class ViewTransactionListAdapter extends ArrayAdapter {
             amount.setText("$"+transactionItemList.get(position).getAmount());
 
             TextView date = (TextView) convertView.findViewById(R.id.get_date);
-            date.setText(transactionItemList.get(position).getTransactionDate());
+            String formattedDate = fromUTCToString(transactionItemList.get(position).getTransactionDate());
+            date.setText(formattedDate);
 
+//
             ImageView groupIcon = (ImageView)convertView.findViewById(R.id.get_group_icon);
             int imageId = GroupToImage.getImage(transactionItemList.get(position).getGroupName());
             groupIcon.setImageResource(imageId);
@@ -47,4 +55,26 @@ public class ViewTransactionListAdapter extends ArrayAdapter {
         }
             return convertView;
         }
+
+    private String fromUTCToString(String utcDate)
+    {
+        String[] months =  context.getResources().getStringArray(R.array.months);
+        String[]weekdays = context.getResources().getStringArray(R.array.weekdays);
+        String dateString = null;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        try {
+            Date date = simpleDateFormat.parse(utcDate);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            dateString = (weekdays[calendar.get(Calendar.DAY_OF_WEEK)] +","
+                    +calendar.get(Calendar.DAY_OF_MONTH))+ " "
+                    +months[calendar.get(Calendar.MONTH)]+" "
+                    +calendar.get(Calendar.YEAR);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return dateString;
+
+    }
     }
